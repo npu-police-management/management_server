a5ecebc7fe901ff2998097efadc3408a5f4b2027
耿耿耿1100 <3349495429@qq.com>
2023-01-28 22:04:17 +0800

    添加日志切片

--- src/main/java/com/nwpu/managementserver/ManagementServerApplication.java
+++ src/main/java/com/nwpu/managementserver/ManagementServerApplication.java
@@ -8,6 +8,32 @@ public class ManagementServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManagementServerApplication.class, args);
+		System.out.println("""
+				       ┌─┐       ┌─┐ + +
+				    ┌──┘ ┴───────┘ ┴──┐++
+				    │                 │
+				    │       ───       │++ + + +
+				    ███████───███████ │+
+				    │                 │+
+				    │       ─┴─       │
+				    │                 │
+				    └───┐         ┌───┘
+				        │         │
+				        │         │   + +
+				        │         │
+				        │         └──────────────┐
+				        │                        │
+				        │                        ├─┐
+				        │                        ┌─┘
+				        │                        │
+				        └─┐  ┐  ┌───────┬──┐  ┌──┘  + + + +
+				          │ ─┤ ─┤       │ ─┤ ─┤
+				          └──┴──┘       └──┴──┘  + + + +
+				                 神兽保佑
+				                代码无BUG!
+				              项目成功跑起来啦！
+				\s""");
+
 	}
 
 }
